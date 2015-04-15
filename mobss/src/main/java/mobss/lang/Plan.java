535f538fbc917e88ec50f2f27c0943f073c74f6d
Dhirendra Singh <dhi.singh@gmail.com>
2015-04-15 16:44:19 +1000

    Updated to use maven license plugin

--- mobss/src/main/java/mobss/lang/Plan.java
+++ mobss/src/main/java/mobss/lang/Plan.java
@@ -1,25 +1,26 @@
-/*
-    MOBSS, Masses of BDI Agents for Social Simulation
-    Copyright (C) 2014 Dhirendra Singh
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Contact: Dhirendra Singh <dhi.singh@gmail.com>
-*/
-
-
 package mobss.lang;
+
+/*
+ * #%L
+ * MOBSS, Masses of BDI-agents for Social Simulation
+ * %%
+ * Copyright (C) 2014 - 2015 by its authors. See AUTHORS file.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
 
 import mobss.struct.AStackObject;
 
