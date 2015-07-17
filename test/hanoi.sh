8f9f20d665c9faf53ebd4163ca2bcaea50468b06
Dhirendra Singh <dhi.singh@gmail.com>
2015-07-17 11:02:43 +1000

    Updated hanoi test run

--- test/hanoi.sh
+++ test/hanoi.sh
@@ -7,12 +7,12 @@ CP=${DIR}/../jill/target/jill-0.0.1-SNAPSHOT-jar-with-dependencies.jar:${DIR}/..
 CMD="java -cp ${CP} agentsoz.jill.Main -h"
 echo $CMD; eval $CMD
 
-DISCS=9
+DISCS=15
 OUTFILE=${DIR}/hanoi.out
 echo ""
 echo "Solving Towers of Hanoi with ${DISCS} discs (see ${OUTFILE})"
-CMD="java -cp ${CP} agentsoz.jill.Main --agent-class agentsoz.jill.example.hanoi.Player --logfile ${DIR}/hanoi.log --debug-level WARN --program-args \"-discs ${DISCS}\" --num-agents 1 --outfile ${OUTFILE}" 
+CMD="java -cp ${CP} agentsoz.jill.Main --agent-class agentsoz.jill.example.hanoi.Player --logfile ${DIR}/hanoi.log --debug-level INFO --program-args \"-discs ${DISCS}\" --num-agents 1 --outfile ${OUTFILE}" 
 echo "Started at " `date`
-echo $CMD; eval $CMD
+echo $CMD; eval $CMD > /dev/null
 echo "Finished at" `date`
 
