eab282d20f99597904f297b731a87b3e6cdd9176
Dhirendra Singh <dhi.singh@gmail.com>
2017-03-24 11:48:14 +1100

    updated script in anticipation of next release

--- test/greeter.sh
+++ test/greeter.sh
@@ -2,7 +2,7 @@
 
 DIR=`dirname "$0"`
 
-CP=${DIR}/../jill/target/jill-0.3.2-SNAPSHOT-jar-with-dependencies.jar:${DIR}/../examples/target/jill-examples-0.3.2-SNAPSHOT.jar
+CP=${DIR}/../jill/target/jill-0.3.2-jar-with-dependencies.jar:${DIR}/../examples/target/jill-examples-0.3.2.jar
 
 # Connect the BDI system
 CMD="java -cp ${CP} io.github.agentsoz.jill.Main --help"
--- test/tokenpassing.sh
+++ test/tokenpassing.sh
@@ -5,7 +5,7 @@ DIR=`dirname "$0"`
 OUTDIR=${DIR}/tokenpassing-results
 mkdir -p ${OUTDIR}
 
-CP=${DIR}/../jill/target/jill-0.3.2-SNAPSHOT-jar-with-dependencies.jar:${DIR}/../examples/target/jill-examples-0.3.2-SNAPSHOT.jar
+CP=${DIR}/../jill/target/jill-0.3.2-jar-with-dependencies.jar:${DIR}/../examples/target/jill-examples-0.3.2.jar
 
 CMD="java -cp ${CP} io.github.agentsoz.jill.Main --help"
 echo $CMD; $CMD
