6fdd002e10f292f05a8c4608e37b2def1027c639
Dhirendra Singh <dhi.singh@gmail.com>
2017-08-10 09:51:34 +1000

    Updated coverage update script for codacy

--- qa/update-codacy.sh
+++ qa/update-codacy.sh
@@ -2,6 +2,6 @@
 
 DIR=$(dirname "$0")
 
-CMD="cd $DIR/../jill && mvn com.gavinmogan:codacy-maven-plugin:coverage -DcoverageReportFile=target/site/jacoco/jacoco.xml -DprojectToken=884cdc9110044d2e9efd91854ead6c82 -DapiToken=884cdc9110044d2e9efd91854ead6c82"
+CMD="cd $DIR/.. && mvn -N com.gavinmogan:codacy-maven-plugin:coverage -DcoverageReportFile=target/coverage-reports/jacoco/jacoco.xml -DprojectToken=884cdc9110044d2e9efd91854ead6c82 -DapiToken=884cdc9110044d2e9efd91854ead6c82"
 echo $CMD
 eval $CMD
