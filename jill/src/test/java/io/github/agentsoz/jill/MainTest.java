a02dd9f09ba5634d924be8506766d5052206e97d
Dhirendra Singh <dhi.singh@gmail.com>
2017-11-06 06:45:14 +1100

    Added test for reproducible logging from multiple agents

--- jill/src/test/java/io/github/agentsoz/jill/MainTest.java
+++ jill/src/test/java/io/github/agentsoz/jill/MainTest.java
@@ -154,4 +154,25 @@ public class MainTest {
     Main.main(args);
     assertEquals(output, out.toString());
   }
+  
+  @Test
+  public void testMultiagentReproducableLogging() {
+    final String output = "a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 " 
+            + "a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 "
+            + "a20 a21 a22 a23 a24 a25 a26 a27 a28 a29 "
+            + "a30 a31 a32 a33 a34 a35 a36 a37 a38 a39 "
+            + "a40 a41 a42 a43 a44 a45 a46 a47 a48 a49 "
+            ;
+    final String[] args = {"--plan-selection-policy", "FIRST", 
+        "--config", "{" 
+            + "randomSeed:123456," 
+            + "numThreads:1," 
+            + "agents:["
+            + "{classname:io.github.agentsoz.jill.testprogram.LogAgent,"
+            + " args:[\"-d\"], count:50}" + "]" 
+            + "}"};
+    Main.main(args);
+    assertEquals(output, out.toString());
+  }
+
 }
---
+++ jill/src/test/java/io/github/agentsoz/jill/testprogram/LogPlan.java
@@ -0,0 +1,48 @@
+package io.github.agentsoz.jill.testprogram;
+
+/*
+ * #%L Jill Cognitive Agents Platform %% Copyright (C) 2014 - 2017 by its authors. See AUTHORS file.
+ * %% This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>. #L%
+ */
+
+import io.github.agentsoz.jill.lang.Agent;
+import io.github.agentsoz.jill.lang.Goal;
+import io.github.agentsoz.jill.lang.Plan;
+import io.github.agentsoz.jill.lang.PlanStep;
+
+import java.util.Map;
+
+public class LogPlan extends Plan {
+
+  public LogPlan(Agent agent, Goal goal, String name) {
+    super(agent, goal, name);
+    body = steps;
+  }
+
+  public boolean context() {
+    return true;
+  }
+
+  PlanStep[] steps = {
+    new PlanStep() {
+      public void step() {
+        System.out.print(getAgent().getName() + " ");
+      }
+    },
+  };
+
+  @Override
+  public void setPlanVariables(Map<String, Object> vars) {
+    // TODO Auto-generated method stub
+
+  }
+}
