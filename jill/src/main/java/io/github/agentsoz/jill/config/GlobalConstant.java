9cab929af9482417929a21cad593b2286a756393
Dhirendra Singh <dhi.singh@gmail.com>
2018-08-17 12:02:50 +1000

    Updated copyright yeas in command line usage text

--- jill/src/main/java/io/github/agentsoz/jill/config/GlobalConstant.java
+++ jill/src/main/java/io/github/agentsoz/jill/config/GlobalConstant.java
@@ -30,7 +30,7 @@ public class GlobalConstant {
   public static final int NULLID = -1;
   /** Constant <code>APP_HEADER="Jill Cognitive Agents Platform\n(C) 201"{trunked}</code>. */
   public static final String APP_HEADER =
-      "Jill Cognitive Agents Platform" + "\n" + "(C) 2014 - 2015 by its authors. See AUTHORS file."
+      "Jill Cognitive Agents Platform" + "\n" + "(C) 2014 - 2018 by its authors. See AUTHORS file."
           + "\n\n" + "This program comes with ABSOLUTELY NO WARRANTY." + "\n"
           + "This is free software, and you are welcome to redistribute it" + "\n"
           + "under certain conditions; for details see LICENSE file provided.";
